8c606d5767682c37789a67c41a00fae952900f1d
wheat <ryoojimu@gmail.com>
2020-07-02 19:18:52 -0400

    Initial Commit

---
+++ Minimal HR monitor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Minimal HR monitor
+//
+//  Created by Jimu Ryoo on 7/2/20.
+//  Copyright © 2020 Jimu Ryoo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
